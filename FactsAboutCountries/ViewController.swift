3efbda7e535dd3faf7030b2bd0c0989d2f666b69
Roman Cebula <ceboolion@gmail.com>
2020-05-17 16:43:57 +0200

    change/ creation of basic TableView

--- FactsAboutCountries/ViewController.swift
+++ FactsAboutCountries/ViewController.swift
@@ -9,15 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let cellID = "Cell"
+    
+    private let tableView: UITableView = {
+       let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.backgroundColor = .black
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: cellID)
         configureView()
     }
 
     private func configureView(){
         view.backgroundColor = .white
+        view.addSubview(tableView)
+        tableView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        return cell
+    }
 }
 
