7da3f05044599f37a5068354b6a7f6d02438b657
Roman Cebula <ceboolion@gmail.com>
2020-05-18 09:44:45 +0200

    model/ creation  of a model for chosen country

---
+++ FactsAboutCountries/Model/SelectedCountry.swift
@@ -0,0 +1,33 @@
+//
+//  SelectedCountry.swift
+//  FactsAboutCountries
+//
+//  Created by Roman Cebula on 18/05/2020.
+//  Copyright © 2020 Ceboolion. All rights reserved.
+//
+
+import Foundation
+
+struct SelectedCountry: Codable {
+  let name: String
+  let capital: String
+  let region: String
+  let subregion: String
+  let population: Int
+  let area: Int
+  let timezone: [String]
+  let currencies: [Currency]
+  let languages: [Language]
+  let flag: String
+}
+
+struct Currency: Codable {
+  let code: String
+  let name: String
+  let symbol: String
+}
+
+struct Language: Codable {
+  let name: String
+  let nativeName: String
+}
