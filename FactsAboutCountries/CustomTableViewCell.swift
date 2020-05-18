3111a698411a7aefd3023be14898ed43d3f344e4
Roman Cebula <ceboolion@gmail.com>
2020-05-18 09:54:04 +0200

    change/ addition of a title within detailVC for a chosen country, Custom View Cell cleanup

--- FactsAboutCountries/CustomTableViewCell.swift
+++ FactsAboutCountries/CustomTableViewCell.swift
@@ -10,15 +10,11 @@ import UIKit
 
 class CustomTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 }
--- FactsAboutCountries/View/DetailViewController.swift
+++ FactsAboutCountries/View/DetailViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+  var country = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = country
         view.backgroundColor = .white
     }
 }
